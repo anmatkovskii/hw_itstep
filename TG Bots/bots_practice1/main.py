a10e9767678aefce702cf2a3fa78959ae9189ff6
anmatkovskii <anmatkovskii@gmail.com>
2022-12-28 14:59:50 +0200

    Final practice Bot

--- TG Bots/bots_practice1/main.py
+++ TG Bots/bots_practice1/main.py
@@ -2,11 +2,17 @@ import telebot
 import os.path
 from random import choice
 from telebot import types
+import base64
 
 config = {
     'name': 'itstep_practice_bot',
     'token': '5879283468:AAFSZVH6zDPeN2eqIoLZCpvDKS4IvbETG6c'
 }
+
+login_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+button = types.KeyboardButton("Login")
+login_keyboard.add(button)
+
 
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 button = types.KeyboardButton("Hello")
@@ -17,59 +23,29 @@ button = types.KeyboardButton("Calculate")
 keyboard.add(button)
 button = types.KeyboardButton("Stats")
 keyboard.add(button)
+button = types.KeyboardButton("Photo encode")
+keyboard.add(button)
+button = types.KeyboardButton("Exit profile")
+keyboard.add(button)
 
-# keyboard_stats = types.ReplyKeyboardMarkup(resize_keyboard=True)
-# button = types.KeyboardButton("Words")
-# keyboard_stats.add(button)
-# button = types.KeyboardButton("Symbols")
-# keyboard_stats.add(button)
-# button = types.KeyboardButton("Digits")
-# keyboard_stats.add(button)
-# button = types.KeyboardButton("Vowels")
-# keyboard_stats.add(button)
-# button = types.KeyboardButton("Consonants")
-# keyboard_stats.add(button)
-# button = types.KeyboardButton("Punctuation marks")
-# keyboard_stats.add(button)
 
 bot = telebot.TeleBot(config["token"])
-
+photo_list = []
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "/start", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Login first!", reply_markup=login_keyboard)
 
 
 @bot.message_handler(content_types=["text"])
 def get_text(message):
-    if message.text == "Magic Ball":
-        question = bot.send_message(message.chat.id, "What's your question?")
-        bot.register_next_step_handler(question, magic_ball)
-
-    elif message.text == "Hello":
-        bot.send_message(message.chat.id, f"Hello, {message.chat.first_name}")
-
-    elif message.text == "Calculate":
-        question = bot.send_message(message.chat.id, "What's your equation?")
-        bot.register_next_step_handler(question, calculator)
-
-    elif message.text == "Stats":
-        # stats_choose = bot.send_message(message.chat.id, "What type of stats do you need?", reply_markup=keyboard_stats)
-        # bot.register_next_step_handler(stats_choose, stats_type)
-        question = bot.send_message(message.chat.id, "What's your message?")
-        bot.register_next_step_handler(question, stats)
-
-    elif message.text == "Pal":
-        question = bot.send_message(message.chat.id, "What's your word?")
-        bot.register_next_step_handler(question, check_palindrome)
-
-    elif message.text == "Even":
-        question = bot.send_message(message.chat.id, "What's your number?")
-        bot.register_next_step_handler(question, check_even)
-
-    elif message.text == "Login":
+    if message.text == "Login":
         question = bot.send_message(message.chat.id, "Enter your login and password: [Login Password]")
         bot.register_next_step_handler(question, auth)
+
+    else:
+        bot.send_message(message.chat.id, "Login first!", reply_markup=login_keyboard)
+
 
 
 def auth(message):
@@ -78,7 +54,42 @@ def auth(message):
         with open("logins.txt", "r") as file:
             data = file.read()
             if message.text in data:
-                bot.send_message(message.chat.id, "Login successful")
+                action1 = bot.send_message(message.chat.id, "Login successful", reply_markup=keyboard)
+                def action(message):
+                    if message.text == "Magic Ball":
+                        question = bot.send_message(message.chat.id, "What's your question?")
+                        bot.register_next_step_handler(question, magic_ball)
+
+                    elif message.text == "Photo encode":
+                        question = bot.send_message(message.chat.id, "What's your photo?")
+                        bot.register_next_step_handler(question, get_user_pics)
+
+                    elif message.text == "Hello":
+                        bot.send_message(message.chat.id, f"Hello, {message.chat.first_name}")
+
+                    elif message.text == "Calculate":
+                        question = bot.send_message(message.chat.id, "What's your equation?")
+                        bot.register_next_step_handler(question, calculator)
+
+                    elif message.text == "Stats":
+                        # stats_choose = bot.send_message(message.chat.id, "What type of stats do you need?", reply_markup=keyboard_stats)
+                        # bot.register_next_step_handler(stats_choose, stats_type)
+                        question = bot.send_message(message.chat.id, "What's your message?")
+                        bot.register_next_step_handler(question, stats)
+
+                    elif message.text == "Pal":
+                        question = bot.send_message(message.chat.id, "What's your word?")
+                        bot.register_next_step_handler(question, check_palindrome)
+
+                    elif message.text == "Even":
+                        question = bot.send_message(message.chat.id, "What's your number?")
+                        bot.register_next_step_handler(question, check_even)
+
+                    elif message.text == "Exit profile":
+                        bot.send_message(message.chat.id, "Login to reach other bot functions!", reply_markup=login_keyboard)
+
+                bot.register_next_step_handler(action1, action)
+
             elif log_and_pass[0] in data:
                 bot.send_message(message.chat.id, "Wrong password")
             else:
@@ -94,16 +105,30 @@ def auth(message):
                                 with open("logins.txt", "w") as file:
                                     file.write(data)
                                     file.write(message.text + "\n")
-                                bot.send_message(message.chat.id, "Register successful")
+                                bot.send_message(message.chat.id, "Register successful! Login now!")
                             else:
                                 bot.send_message(message.chat.id, "Wrong register form")
                         bot.register_next_step_handler(question2, reg)
+                    else:
+                        bot.send_message(message.chat.id, "Login to reach other bot functions!", reply_markup=login_keyboard)
 
                 bot.register_next_step_handler(question, reg_or_not)
 
     except:
         bot.send_message(message.chat.id, "Error")
 
+def get_user_pics(message):
+    if message.content_type == "photo":
+        file_info = bot.get_file(message.photo[-1].file_id)
+        download_file = bot.download_file(file_info.file_path)
+        with open("photo.jpg", "wb") as photo:
+            photo.write(download_file)
+        with open("photo.jpg", "rb") as photo:
+            photo_encode = base64.b64encode(photo.read()).decode('utf-8')
+        with open("image.txt", "w") as file:
+            file.write(photo_encode)
+        with open("image.txt", "r") as file:
+            bot.send_document(message.chat.id, file)
 
 def check_palindrome(message):
     word = message.text
@@ -111,6 +136,7 @@ def check_palindrome(message):
         bot.send_message(message.chat.id, "Palindrome")
     else:
         bot.send_message(message.chat.id, "Not Palindrome")
+    bot.send_message(message.chat.id, "Any actions?", reply_markup=keyboard)
 
 
 def check_even(message):
@@ -118,6 +144,7 @@ def check_even(message):
         bot.send_message(message.chat.id, "Even")
     elif int(message.text) % 2 == 1:
         bot.send_message(message.chat.id, "Odd")
+    bot.send_message(message.chat.id, "Any actions?", reply_markup=keyboard)
 
 
 def stats(message):
@@ -148,6 +175,7 @@ def stats(message):
     with open("stat.txt", "r") as file:
         bot.send_document(message.chat.id, file)
         file.close()
+    bot.send_message(message.chat.id, "Any actions?", reply_markup=keyboard)
 
 
 def magic_ball(message):
@@ -158,7 +186,7 @@ def magic_ball(message):
                                               "Concentrate and ask again.", "Better not tell you now.",
                                               "Don't count on it.", "My reply is no.", "My sources say no.",
                                               "Outlook not so good.", "Very doubtful."]))
-
+    bot.send_message(message.chat.id, "Any actions?", reply_markup=keyboard)
 
 def calculator(message):
     equation = message.text + "!"
@@ -233,5 +261,6 @@ def calculator(message):
         elif "/" in list_of_nums:
             end_num = str(float(list_of_nums[0])/float(list_of_nums[2]))
             bot.send_message(message.chat.id, end_num)
+    bot.send_message(message.chat.id, "Any actions?", reply_markup=keyboard)
 
 bot.polling(none_stop=True, interval=0)
